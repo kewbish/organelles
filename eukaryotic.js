14c234dfc007558e337052a28b0b054db900ccd4
Kewbish <kewbish@gmail.com>
2020-01-29 20:02:50 -0800

    SVG system built

---
+++ eukaryotic.js
@@ -0,0 +1,17 @@
+// document.getElementById("eukaria-interactive-text").innerHTML = "X";
+
+function cell_membrane(){
+    document.getElementById("eukaria-interactive-text").innerHTML = "The cellular membrane - Eukaria's main line of defense.";
+}
+
+function cytoplasm(){
+    document.getElementById("eukaria-interactive-text").innerHTML = "The cytoplasm - Eukaria's unfortunate breathing situation.";
+}
+
+function centrioles(){
+    document.getElementById("eukaria-interactive-text").innerHTML = "The centrioles - The spiderweavers of Eukaria.";
+}
+
+function mitochondria(){
+    document.getElementById("eukaria-interactive-text").innerHTML = "The mitochondria - The powerhouse of the cell™";
+}
