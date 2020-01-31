3b207ff73cb6e85d1e72394d27bb95f08b10d8af
Kewbish <kewbish@gmail.com>
2020-01-30 20:03:05 -0800

    Scaffolding work

--- eukaryotic.js
+++ eukaryotic.js
@@ -15,3 +15,47 @@ function centrioles(){
 function mitochondria(){
     document.getElementById("eukaria-interactive-text").innerHTML = "The mitochondria - The powerhouse of the cell™";
 }
+
+function ribosome() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function lysosome() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function nucleus() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function nuclear_pore() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function chromatin() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function rough_er() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function smooth_er() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function centrosome() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function flagella() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function golgi_apparatus() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
+
+function golgi_vesicle() {
+    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+}
