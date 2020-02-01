8be789f31196f7a03831eaef86feb742bb4e5c1f
Kewbish <kewbish@gmail.com>
2020-01-31 19:50:54 -0800

    Work on Eukaryotic page

--- eukaryotic.js
+++ eukaryotic.js
@@ -1,27 +1,27 @@
 // document.getElementById("eukaria-interactive-text").innerHTML = "X";
 
 function cell_membrane(){
-    document.getElementById("eukaria-interactive-text").innerHTML = "The cellular membrane - Eukaria's main line of defense.";
+    document.getElementById("eukaria-interactive-text").innerHTML = "<b>The cellular membrane - Eukaria's main line of defense.</b><br>The cellular membrane serves as the border of Eukaria, staffed by our finest police officers. These officers are trained to scrutinize exactly what comes in: only the essentials are allowed, including water, oxygen, and carbon dioxide.<br>Initially, our borders were only one line, but further research apparently showed that membranes were hydrophilic and phobic zones sandwiched between proteins. Currently, we have a model of proteins, both hydrophilic and hydrophobic, dispersed throughout the border. In between the proteins, phospholipids make up the rest of the border protection forces. These phospholipids are in two lines, orienting their hydrophilic heads to water and their hydrophobic parts towards each other.<br>Contrary to popular belief, our border services are flexible and fluid. Cholesterol (another type of lipid) controls the exact amount of fluidity. At medium temperatures, they reduce movement, but hinder membrane solidification at lower temperatures by disrupting the phospholipids's connections.";
 }
 
 function cytoplasm(){
-    document.getElementById("eukaria-interactive-text").innerHTML = "The cytoplasm - Eukaria's unfortunate breathing situation.";
+    document.getElementById("eukaria-interactive-text").innerHTML = "<b>The cytoplasm - Eukaria's unfortunate breathing situation.</b><br>Inside Eukaria, we live in cytoplasm, not air. Cytoplasm is a thick substance, formed of a mixture of mainly water, salts, and proteins. It surrounds all the other organelles, and whatever is outside of an organelle is called cytosol (a watery substance that makes up most of the cytoplasm).<br>Though it seems unorganized, cytoplasm's proteins (called the cytoskeleton) actually provide a very structured existence for Eukaria. This cytoskeleton helps the cell maintain its form when moving or dividing.";
 }
 
 function centrioles(){
-    document.getElementById("eukaria-interactive-text").innerHTML = "The centrioles - The spiderweavers of Eukaria.";
+    document.getElementById("eukaria-interactive-text").innerHTML = "<b>The centrioles - The spiderweavers of Eukaria.</b><br>Located in the centrosome, a cell has several types of microtubules, such as the centrioles. Microtubules help support the cell when organelles are moving, and also play a role in separating chromosomes during cell division. Centrioles help organize fibers during cell division. When a cell is divided, centrioles are 'reelected' and replicated into the new cell.<br>Centrioles are rotated at ninety-degrees to each other, and there are only two in a cell. {My diagram is technically incorrect as centrioles should have 9 spokes with three microtubules, while my diagram shows 8. They are also not shown at ninety-degrees due to the limitations of 2D space.}<br>Centrioles aren't essential to a city; plant cells don't have centrioles, and many eukaryotes don't require this function either. However, they help in the process of mitosis, and therefore make Eukaria superior, as an owner of our two dear centrioles.";
 }
 
 function mitochondria(){
-    document.getElementById("eukaria-interactive-text").innerHTML = "The mitochondria - The powerhouse of the cell™";
+    document.getElementById("eukaria-interactive-text").innerHTML = "<b>The mitochondria - The powerhouse of the cell™</b><br>Tiny (about 1 - 10µm large) but strong, mitochrondia produce energy to fuel Eukaria. Inside these mini-factories, enzymes chug away to break fuels down into energy. Cellular respiration in the mitochrondia produce ATP (Eukaria's source of fuel and energy) through extracting energy from sugars, fats, and other power sources.<br><br>The outer membrane of the mitrochrondia is smooth, but the inner membrane is folded and wrinkled. This inner membrane, called the cristae, houses the mitochrondial matrix. The matrix contains enzymes, DNA, and ribosomes, which work through cellular respiration. Our factory has a relatively small volume, but a huge inner surface area. The folds in the inner membrane increase its surface area, thereby also increasing its productivity in powering Eukaria.";
 }
 
 function ribosome() {
-    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+    document.getElementById("eukaria-interactive-text").innerHTML = "<b>The ribosome - Amazon for cells</b><br>Eukaria's a thriving cities, with many citizens constantly buying consumer goods. Ribosomes feed that capitalism, generating essential proteins to placate the masses. Ribosomes produce different proteins depending on where they are. For example, ribosomes in the cytosol produce enzymes that will help in breaking sugars down for later processing.<br>DNA is present in genes, which are transcribed to a messenger DNA, called RNA. Ribosomes take this information from RNA and synthesizes a protein that has a certain function, depending on what's in the RNA. That protein will go to the organelles to help with a certain job in Eukaria.";
 }
 
 function lysosome() {
-    document.getElementById("eukaria-interactive-text").innerHTML = "X";
+    document.getElementById("eukaria-interactive-text").innerHTML = "<b>The lysosome - The waste disposal system</b><br>In Eukaria, we pride ourselves on an efficient garbage system. Lysosomes are sacs of enzymes used to digest proteins or other waste molecules in the cell. Inside, proteins, fats, and nucleic acids are broken down with water in a process called hydrolyzation.";
 }
 
 function nucleus() {
